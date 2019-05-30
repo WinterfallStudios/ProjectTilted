2926dac400e57c64c23493110392f237f18cd8c8
WinterfallStudios <fransw104@gmail.com>
2019-05-30 08:31:17 +0200

    g

--- project tilt/ProjectTilted/Assets/pickup.cs
+++ project tilt/ProjectTilted/Assets/pickup.cs
@@ -8,18 +8,20 @@ public class pickup : MonoBehaviour
     public float throwforce = 10f;
     private Rigidbody rb;
     private int times;
+    public Collider cc;
     // Start is called before the first frame update
     void Start()
     {
         times = 0;
         rb = GetComponent<Rigidbody>();
         dest = GameObject.Find("throwpos").transform;
+        cc = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (times >= 4)
+        if (times >= 6)
         {
             Destroy(gameObject);
         }
@@ -29,12 +31,14 @@ public class pickup : MonoBehaviour
         GetComponent<Rigidbody>().useGravity = false;
         transform.rotation = dest.rotation;
         this.transform.parent = GameObject.Find("throwpos").transform;
+        cc.isTrigger = true;
 
     }
     private void OnMouseUp()
     {
+        cc.isTrigger = false;
         times++;
-        transform.Translate(0f, 0f, throwforce);
+        transform.Translate(0f, 0f, 5f);
         this.transform.parent = null;
         GetComponent<Rigidbody>().useGravity = true;
     }
